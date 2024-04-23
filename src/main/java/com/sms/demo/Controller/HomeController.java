01dca3cea7b999135859c201bd940a36dbd5e8f6
NAVEEN <2200080142@kluniversity.in>
2024-04-23 22:45:26 +0530

    4th change

--- src/main/java/com/sms/demo/Controller/HomeController.java
+++ src/main/java/com/sms/demo/Controller/HomeController.java
@@ -1,14 +1,11 @@
 package com.sms.demo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
         System.err.println("Home Controller");
-        return "home1.html"; // Assuming your HTML file is named home1.html
-    }
-   
+        return "index"; // Assuming your HTML file is named home1.html
+    }  
 }
